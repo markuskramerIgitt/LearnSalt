64c16e3d9a16c71802e6240c36a4d54bbb9a141a
Markus Kramer <markuskramerIgitt@gmail.com>
2020-05-31 17:53:48 +0200

    m

--- profile_salt-bat0.bat
+++ profile_salt-bat0.bat
@@ -1,1 +1,1 @@
-pycallgraph -v graphviz -- /Python35/Scripts/salt-call --local file.replace /git/LearnSalt/load_profiler_replace.txt pattern="you3" repl="you2" backup=False
+pycallgraph -v graphviz -- /Python35/Scripts/salt-call --local file.replace /git/LearnSalt/file_replace.txt pattern="you3" repl="you2" backup=False
