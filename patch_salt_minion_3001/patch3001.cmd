a27b913ead0516dae4d3b95fc151e4f8e8e91013
Markus Kramer <markuskramerIgitt@gmail.com>
2020-08-14 20:34:19 +0200

    patch_salt_minion_3001

---
+++ patch_salt_minion_3001/patch3001.cmd
@@ -0,0 +1,8 @@
+copy config\__init__.py  c:\git\salt\salt\config\__init__.py
+copy modules\cmdmod.py   c:\git\salt\salt\modules\cmdmod.py
+copy utils\chcp.py       c:\git\salt\salt\utils\chcp.py
+
+
+
+
+
